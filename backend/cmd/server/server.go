265e90b1d6a054bd25cc5881b037186a2f92fc80
Zayaan Rahman <rahmanzayaan43@gmail.com>
2023-06-04 22:17:25 -0400

    Added command

---
+++ backend/cmd/server/server.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+
+// This command starts a server (e.g. a peer) in our network.
+// Any peer is capable of sending/receiving a message.
+func main() {
+	fmt.Println("Hello world")
+}
