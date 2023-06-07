0d5e9e8e04d0da42ef18b6e0c9151970f20c666f
Zayaan Rahman <rahmanzayaan43@gmail.com>
2023-06-07 14:07:13 -0400

    Moved RED/red.go to api/red.go

--- backend/api/red.go
+++ backend/api/red.go
@@ -1,5 +1,13 @@
 package api
 
+import (
+	"errors"
+	"net"
+	"io"
+	"log"
+)
+
+
 type REDServer interface {
 	// Sends the given message to the given peer
 	// It returns an error if sending the message failed and nil if it succeeded
@@ -11,4 +19,50 @@ type REDServer interface {
 	// Terminate the server, closing the listening socket
 	// on this server.
 	Terminate()
+}
+
+type RServer struct {
+	// The address for this server
+	addr string
+
+	// The listener socket for this server.
+	// It listens for incoming connections.
+	ln net.Listener
+}
+
+// Create a new RED server associated with the given address.
+// The newly created RED server begins send or receive messages immedaiately.
+// This function returns an error if the server was not able to be created.
+func NewREDServer(addr string) (REDServer, error) {
+	// TODO - Should we use TCP or UDP?
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	rs := &RServer{addr, ln}
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			// TODO - If error upon accepting a connection, how should we handle it
+			if err != nil {
+				return;
+			}
+			defer conn.Close()
+			buffer := make([]byte, 24)
+			bytes, _ := io.ReadFull(conn,buffer)
+			log.Printf("Received a message: %v\n", bytes)
+		}
+	}()
+	return rs, nil
+}
+
+func (rs *RServer) Send(addr string) error {
+	return errors.New("TODO")
+}
+
+func (rs *RServer) Recv() {
+}
+
+func (rs *RServer) Terminate() {
+	rs.ln.Close()
 }
