0dab9793c67054d4863ff2f797fa350858cf4a77
Zayaan Rahman <rahmanzayaan43@gmail.com>
2023-06-15 17:04:36 -0400

    updated pyscript and index

--- frontend/main.py
+++ frontend/main.py
@@ -1,1 +1,4 @@
-print("I am using PyScript this print this message")
+# TODO - Use socket library to send proto3 messages to the golang side.
+# TODO - Use selenium. We can use selenium to continously fetch updates from the text document the user is currently editing.
+# We will send those updates to our Golang backend.
+
