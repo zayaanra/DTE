be9a48872750fd982a635050021c477dc81bfda8
Zayaan Rahman <rahmanzayaan43@gmail.com>
2023-06-18 15:53:34 -0400

    Updated to use bs4/requests instead of selenium

--- frontend/main.py
+++ frontend/main.py
@@ -2,3 +2,15 @@
 # TODO - Use selenium. We can use selenium to continously fetch updates from the text document the user is currently editing.
 # We will send those updates to our Golang backend.
 
+from bs4 import BeautifulSoup
+import requests
+
+
+URL = "http://localhost:8080"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+while True:
+    results = soup.find(id="doc")
+    print(results)
