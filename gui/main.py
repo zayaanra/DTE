c8a6bb495d58b46e20eb2650afde85700950efc6
Zayaan Rahman <rahmanzayaan43@gmail.com>
2023-06-21 15:39:59 -0400

    Cleaned up GUI, added methods

--- gui/main.py
+++ gui/main.py
@@ -1,35 +1,54 @@
-import sys
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+import sys
+import socket
 
+# SocketServer class. This will be used to send proto3 messages to the Golang backend.
+class SocketServer():
+    # TODO
+    def __init__(self):
+        self.socket = None
+        
+
+# Main GUI application
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.x, self.y, self.aw, self.ah, = 500, 1000, 1000, 750
+        self.server = SocketServer()
+        self.x, self.y, self.aw, self.ah, = 100, 300, 300, 150
         self.run()
 
     def run(self):
-        self.setGeometry(500, 1000, 1000, 750)
+        # self.setGeometry(self.x, self.y, self.aw, self.ah)
+        self.setMinimumSize(QSize(320, 140))
         self.setWindowTitle("RED")
         
+        self.hostLabel = QLabel('IP Address:', self)
         self.hostBox = QLineEdit(self)
-        self.hostBox.move(250, 100)
-        self.portBox = QLineEdit(self)
-        self.portBox.move(500, 100)
+        self.hostBox.move(100, 20)
+        self.hostBox.resize(200, 32)
+        self.hostLabel.move(20, 20)
 
-        self.hostLabel = QLabel('Host Name (or IP Address)', self)
-        self.hostLabel.adjustSize()
-        self.hostLabel.setBuddy(self.hostBox)
-        self.portLabel = QLabel('Port', self)
-        self.portLabel.setBuddy(self.portBox)
+        self.portLabel = QLabel('Port:', self)
+        self.portBox = QLineEdit(self)
+        self.portBox.move(100, 60)
+        self.portBox.resize(200, 32)
+        self.portLabel.move(20, 60)
+
+        startBtn = QPushButton('Start', self)
+        startBtn.clicked.connect(self.spawn)
+        startBtn.resize(200, 32)
+        startBtn.move(100, 100)
 
         self.show()
     
-
+    # Spawns a Golang process to start the backend server
+    def spawn(self):
+        # TODO
+        print("Button clicked")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     gui = App()
     sys.exit(app.exec_())
-
