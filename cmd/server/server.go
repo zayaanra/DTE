0b05054ce794601fd103bacf22afd63a96dc021c
Zayaan Rahman <rahmanzayaan43@gmail.com>
2023-06-30 16:44:51 -0400

    Added channel for cmd termination

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -17,14 +17,15 @@ func main() {
 	} else {
 		// Parse the port and start a REDServer under that port
 		addr := os.Args[1]
-		rs, err := red.NewREDServer(addr)
+		exit := make(chan bool)
+		rs, err := red.NewREDServer(addr, exit)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "REDServer failed to start up")
 			return
 		}
 		// // Read from stdin until we hit EOF. Then, we can safely terminate our REDServer.
-		os.Stdin.Read(make([]byte, 1))
+		// os.Stdin.Read(make([]byte, 1))
+		<-exit
 		rs.Terminate()
 	}
-
 }
