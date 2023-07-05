8139aafec565608246a32f438fdede547e608c33
Zayaan Rahman <rahmanzayaan43@gmail.com>
2023-07-05 19:07:18 -0400

    Changed approach for spawning server window; added state variables to ServerWindow class; spawning using QtProcess

--- gui/main.py
+++ gui/main.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import *
 
 import sys
 import socket
-import subprocess
+import messages_pb2
 
 # SocketServer class. This will be used to send proto3 messages to the Golang backend.
 class SocketServer():
@@ -14,32 +14,73 @@ class SocketServer():
 
 # ServerWindow class. It's opened when a user begins a new RED server.
 class ServerWindow(QWidget):
-    def __init__(self):
+    def __init__(self, ipAddress):
         super().__init__()
         self.server = None
+        self.process = None
+
+        self.ipAddr = ipAddress
 
         self.x, self.y, self.aw, self.ah, = 100, 300, 300, 150
         self.run()
     
     def run(self):
-        self.setMinimumSize(QSize(320, 140))
-        self.setWindowTitle("RED")
+        if self.process is None:
+            self.setMinimumSize(QSize(320, 140))
+            self.setWindowTitle("RED")
 
-        # TODO - need to create a toolbar
-        # self.toolbar = QToolBar("Test")
-        # self.toolbar.show()
+            # TODO - need to create a toolbar
+            # self.toolbar = QToolBar("Test")
+            # self.toolbar.show()
 
-        self.document = QPlainTextEdit(self)
-        self.document.textChanged.connect(self.fetch)
-        self.document.resize(QSize(320, 140))
-    
+            self.document = QPlainTextEdit(self)
+            self.document.textChanged.connect(self.fetch)
+            self.document.resize(QSize(320, 140))
+
+            self.killBtn = QPushButton('KILL', self)
+            self.killBtn.clicked.connect(self.sendKill)
+
+            # NOTE - we might want to get outputs from stderr and stdout
+            print(f"Server starting under {self.ipAddr}")
+            exec = "../cmd/server/server"
+            self.process = QProcess()
+            self.process.readyReadStandardOutput.connect(self.handleStdout)
+            self.process.readyReadStandardError.connect(self.handleStderr)
+            self.process.finished.connect(self.destroy)
+            self.process.start(exec, [self.ipAddr])
+
+
     # Fetches updates from the text editor
     # When this function is called, we will open a socket server from this Python process and send messages to the backend
     def fetch(self):
         text = self.document.toPlainText()
         print(text)
+    
+    def handleStdout(self):
+        data = self.process.readAllStandardOutput()
+        stderr = bytes(data).decode("utf8")
+        print(stderr)
 
-        
+    def handleStderr(self):
+        data = self.process.readAllStandardError()
+        stderr = bytes(data).decode("utf8")
+        print(stderr)
+    
+    def sendKill(self):
+        # TODO - not sure if im correctly creating these messages
+        smsg = messages_pb2.REDMessage()
+        smsg.type = messages_pb2.KILL
+
+        # TODO - figure out how to connect to the ip address and send protobuf message
+        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        # s.connect(("localhost", 3000))
+        # s.send(smsg.SerializeToString())
+        # s.close()
+
+    # Destroy the server once it's been terminated
+    def destroy(self):
+        print(f"Server under {self.ipAddr} shutting down...")
+        self.process = None
 
 # Main GUI application
 class MainWindow(QMainWindow):
@@ -47,8 +88,9 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         # This is a dictionary because we should be able to spawn as many servers as we want
-        # The key is the IP address and the value is the window associated it with that server
-        self.serverWindows = {}
+        # The key is the IP address.
+        # The value is a tuple holding the ServerWindow and Process object
+        self.serverWindows = []
 
         self.x, self.y, self.aw, self.ah, = 100, 300, 300, 150
         self.run()
@@ -71,32 +113,25 @@ class MainWindow(QMainWindow):
         self.portLabel.move(20, 60)
 
         startBtn = QPushButton('Start', self)
-        startBtn.clicked.connect(self.spawn)
+        startBtn.clicked.connect(self.spawnServerWindow)
         startBtn.resize(200, 32)
         startBtn.move(100, 100)
 
         self.show()
     
-    # Spawns a Golang process to start the backend server
-    def spawn(self):
-        # TODO - spawn the golang process
+    # Spawns a new window for the created server
+    def spawnServerWindow(self):
         host, addr = f"{self.hostBox.text()}", f"{self.portBox.text()}"
         ipAddr = f"{host}:{addr}"
-        # Don't spawn a new server if the address is already being used.
-        # If a user wants to start a new server under an address that is already being used, then they must close the server associated with that address first
+        # Don't spawn a new window if the address is already being used.
+        # If a user wants to start a new window under an address that is already being used, then they must close the window associated with that address first
         if ipAddr in self.serverWindows:
             print("Address already in use")
-        else:
-            print(f"Server starting under {ipAddr}")
-            self.serverWindows[ipAddr] = ServerWindow()
-            self.serverWindows[ipAddr].show()
-            exec = "../cmd/server/server"
-            process = subprocess.Popen([exec, ipAddr], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stderr, stdout = process.communicate()
+        else:            
+            # TODO - if the server under a certain ip address is shutdown, how does this class find out about it?
+            self.serverWindows.append(ServerWindow(ipAddr))
+            self.serverWindows[len(self.serverWindows)-1].show()
 
-            # Print stdout and stderr from spawned process
-            print(stderr.decode())
-            print(stdout.decode())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
