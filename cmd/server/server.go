653f78bde3e9627db7994bef3adf16102d91db34
Zayaan Rahman <rahmanzayaan43@gmail.com>
2023-07-11 21:51:51 -0400

    Updated GUI

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -6,11 +6,12 @@ import (
 	"os"
 
 	"github.com/therecipe/qt/widgets"
+	"github.com/zayaanra/RED/api"
 	"github.com/zayaanra/RED/internal/red"
 )
 
 // Creates the REDServer and begins listening for incoming messages
-func run() {
+func run(comms chan<- api.REDServer) {
 	// TODO - everything below here is the actual server code, it will be used once user clicks server start button
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "usage: ./server port")
@@ -20,22 +21,39 @@ func run() {
 		rs, err := red.NewREDServer(addr)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "REDServer failed to start up")
+			comms <- nil
 			return
 		}
+		comms <- rs
 
 		// Read from stdin until we hit EOF. Then, we can safely terminate our REDServer.
 		os.Stdin.Read(make([]byte, 1))
+		log.Println("Terminating server...")
+		close(comms)
 		rs.Terminate()
 	}
 }
 
-func fetch(text string) {
-	log.Println()
+// Boot REDServer (initialization)
+func boot() api.REDServer {
+	comms := make(chan api.REDServer)
+
+	// Run the server
+	log.Println("Running server...")
+	go run(comms)
+
+	rs := <-comms
+	return rs
 }
 
 // This command starts a server (e.g. a peer) in our network.
 // Any peer is capable of sending/receiving a message.
 func main() {
+	rs := boot()
+	if rs == nil {
+		os.Exit(1)
+	}
+
 	// To test locally, the user should input two arguments to the command line (executable and the port, e.g. "localhost:3000")
 	app := widgets.NewQApplication(0, nil)
 
@@ -60,15 +78,31 @@ func main() {
 		configWindow.SetWindowTitle("Invite Configuration")
 		configWindow.SetModal(true)
 		configWindow.SetFixedSize2(300, 100)
+
+		layout := widgets.NewQVBoxLayout()
+
+		hostLabel := widgets.NewQLabel2("Host", nil, 0)
+		hostEntry := widgets.NewQLineEdit(nil)
+		layout.AddWidget(hostLabel, 0, 0)
+		layout.AddWidget(hostEntry, 0, 0)
+
+		portLabel := widgets.NewQLabel2("Port", nil, 0)
+		portEntry := widgets.NewQLineEdit(nil)
+		layout.AddWidget(portLabel, 0, 0)
+		layout.AddWidget(portEntry, 0, 0)
+
+		btn := widgets.NewQPushButton2("Invite", nil)
+		btn.ConnectClicked(func(bool) {
+			// TODO - We'll send an INVITE message with our RS object.
+		})
+		layout.AddWidget(btn, 0, 0)
+
+		configWindow.SetLayout(layout)
 		configWindow.Show()
 	})
 
 	layout.AddWidget(invite, 0, 0)
 	layout.AddWidget(document, 0, 0)
-
-	// Run the server
-	log.Println("Running server...")
-	go run()
 
 	// Start the GUI
 	window.Show()
