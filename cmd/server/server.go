ac3562f8ac51f79efc083d0399769fb1b2dd44eb
Zayaan Rahman <rahmanzayaan43@gmail.com>
2023-08-20 16:39:29 -0400

    Changed from fyne to gtk

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,6 +4,139 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/gotk3/gotk3/gtk"
+	"github.com/zayaanra/RED/api"
+	"github.com/zayaanra/RED/internal/red"
+)
+
+func run(comms chan<- api.REDServer) {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: ./server port")
+	} else {
+		// Parse the port and start a REDServer under that port
+		addr := os.Args[1]
+		updates := make(chan string)
+		rs, err := red.NewREDServer(addr, updates)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "REDServer failed to start up")
+			comms <- nil
+			return
+		}
+		comms <- rs
+
+		// Read from stdin until we hit EOF. Then, we can safely terminate our REDServer.
+		os.Stdin.Read(make([]byte, 1))
+		log.Println("Terminating server...")
+		close(comms)
+		rs.Terminate()
+	}
+}
+
+func boot() api.REDServer {
+	comms := make(chan api.REDServer)
+
+	// Run the server
+	log.Println("Running server...")
+	go run(comms)
+
+	rs := <-comms
+	return rs
+}
+
+func refresh(doc *gtk.Entry, rs api.REDServer) {
+	updates := rs.Fetch()
+	for text := range updates {
+		doc.SetText(text)
+	}
+}
+
+func findDifference(old, new string) int {
+	var i int
+	for i = 0; i < len(old) && i < len(new); i++ {
+	}
+	return i
+}
+
+func main() {
+	rs := boot()
+	if rs == nil {
+		os.Exit(1)
+	}
+
+	gtk.Init(nil)
+
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
+	}
+	win.SetTitle("RED Editor")
+	win.Connect("destroy", gtk.MainQuit)
+	win.SetDefaultSize(800, 600)
+
+	// Create a vertical box container
+	vBox, err := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 5)
+	if err != nil {
+		log.Fatal("Unable to create box:", err)
+	}
+
+	// Create a toolbar
+	toolbar, err := gtk.ToolbarNew()
+	if err != nil {
+		log.Fatal("Unable to create toolbar:", err)
+	}
+	// Add toolbar actions here (New document, Cut, Copy, Paste, Invite, etc.)
+	vBox.PackStart(toolbar, false, false, 0)
+
+	// Create a scrolled window
+	scrolledWindow, err := gtk.ScrolledWindowNew(nil, nil)
+	if err != nil {
+		log.Fatal("Unable to create scrolled window:", err)
+	}
+	vBox.PackStart(scrolledWindow, true, true, 0)
+
+	// Create a multiline text view
+	textView, err := gtk.TextViewNew()
+	if err != nil {
+		log.Fatal("Unable to create text view:", err)
+	}
+	textBuffer, _ := textView.GetBuffer()
+	scrolledWindow.Add(textView)
+
+	// Create an "Invite" button
+	inviteButton, err := gtk.ButtonNewWithLabel("Invite")
+	if err != nil {
+		log.Fatal("Unable to create button:", err)
+	}
+	vBox.PackStart(inviteButton, false, false, 0)
+
+	// Handle the "Invite" button click event
+	inviteButton.Connect("clicked", func() {
+		// Implement your Invite button logic here
+		fmt.Println("Invite button clicked!")
+	})
+
+	// Connect "changed" signal of the text buffer
+	textBuffer.Connect("changed", func() {
+		// Handle text buffer changes here
+		// You can get the text from the buffer using textBuffer.GetText()
+	})
+
+	// Add the vertical box container to the window
+	win.Add(vBox)
+
+	win.ShowAll()
+	gtk.Main()
+
+}
+
+/*
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
 	"strings"
 
 	"fyne.io/fyne/v2"
@@ -144,3 +277,4 @@ func main() {
 
 	w.ShowAndRun()
 }
+*/
